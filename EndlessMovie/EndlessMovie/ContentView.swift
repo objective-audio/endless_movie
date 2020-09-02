53997fd4e9c9dafe9daae0f2d968d05020f53713
Yuki Yasoshima <yukiyasos@gmail.com>
2020-09-02 23:16:18 +0900

    project

---
+++ EndlessMovie/EndlessMovie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EndlessMovie
+//
+//  Created by Yuki Yasoshima on 2020/09/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
